6242688b5506be9ad15b2e0e15161bb5dcca1056
cnfairall <101303134+cnfairall@users.noreply.github.com>
2023-08-21 12:02:06 -0500

    styling

--- utils/domBuilder.js
+++ utils/domBuilder.js
@@ -1,10 +1,13 @@
 const domBuilder = () => {
   document.querySelector('#app').innerHTML = `
-  <div id= "joke-setup"></div>
-  <div id="joke-punchline"></div>
-  <div id="joke-btn"></div>
-  <div id="punchline-btn"></div>
-  <div id="reset-btn"></div>`;
+  <div id="container">
+    <img src="https://i.pinimg.com/originals/89/3f/f7/893ff70cb2d6f15710321ea492bcbc41.png">
+    <div id= "joke-setup"></div>
+    <div id="joke-punchline"></div>
+    <div id="joke-btn"></div>
+    <div id="punchline-btn"></div>
+    <div id="reset-btn"></div>
+  </div>`;
 };
 
 export default domBuilder;
